62f70b453aa57203797e3dba694ff73b14a50896
lorraine-quantum <quantumoffice010@gmail.com>
2024-02-09 08:45:24 +0100

    currency

--- models/UserModel.js
+++ models/UserModel.js
@@ -12,6 +12,9 @@ const UserSchema = new mongoose.Schema({
   phoneNumber: {
     type: Number,
   },
+  currency: {
+    type: String,
+  },
   gender: {
     type: String,
     default: "Unspecified"
